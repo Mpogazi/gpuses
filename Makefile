0e9c8d2e55d907b81c1d6d1b365a0a00744c3047
Oabraham1 <abrahamojima2018@gmail.com>
2024-03-23 14:01:52 -0400

    Finished db connection

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
 start-client:
 	cd website && pnpm run dev
 
-build-server:
+start-server:
+	cd server && go run cmd/main.go
+
+start-mongo-server:
+	@echo "Starting mongo server"
+	docker run --name gpuse -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=gpuse_secret -d mongo:7.0.0
+
+stop-mongo-server:
+	@echo "Stopping mongo server"
+	docker stop gpuse
+
+format-go:
+	go fmt ./...
---
+++ server/util/config.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYM_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	MongoURI             string        `mapstructure:"MONGO_URI"`
+}
+
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
